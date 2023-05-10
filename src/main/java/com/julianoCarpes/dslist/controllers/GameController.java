88ab878c553c74b4dcba427b4b7cb2a0191dd928
JulianoCarpes <julianocarpes.dev@gmail.com>
2023-05-10 18:53:27 -0300

    Get GameById

--- src/main/java/com/julianoCarpes/dslist/controllers/GameController.java
+++ src/main/java/com/julianoCarpes/dslist/controllers/GameController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.julianoCarpes.dslist.dto.GameMaxDTO;
 import com.julianoCarpes.dslist.dto.GameMinDTO;
 import com.julianoCarpes.dslist.entities.Game;
 import com.julianoCarpes.dslist.services.GameService;
@@ -20,6 +22,12 @@ public class GameController {
 	@GetMapping
 	public List<GameMinDTO> findAll() {
 		List<GameMinDTO> result = gameService.findAll();
+		return result;
+	}
+	
+	@GetMapping(value = "/{id}")
+	public GameMaxDTO findById(@PathVariable Long id) {
+		GameMaxDTO result = gameService.findById(id);
 		return result;
 	}
 }
--- src/main/java/com/julianoCarpes/dslist/services/GameService.java
+++ src/main/java/com/julianoCarpes/dslist/services/GameService.java
@@ -4,20 +4,31 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.julianoCarpes.dslist.dto.GameMaxDTO;
 import com.julianoCarpes.dslist.dto.GameMinDTO;
 import com.julianoCarpes.dslist.entities.Game;
 import com.julianoCarpes.dslist.repositories.GameRepository;
 
 @Service
 public class GameService {
+	
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result=  gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
+	}
+	
+	@Transactional(readOnly = true)
+	public GameMaxDTO findById(Long id){
+		Game result=  gameRepository.findById(id).get();
+		return new GameMaxDTO(result);
+		
 	}
 
 }
